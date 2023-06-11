dd258d49930475bfda56adab01bc2da32d4bb7ef
vasilevskaya <Vasilevskaya@gnivc.ru>
2023-06-12 01:32:54 +0300

    fix style

--- src/pages/Profile/components/Avatar/index.js
+++ src/pages/Profile/components/Avatar/index.js
@@ -5,13 +5,13 @@ import './Avatar.scss';
 export const Avatar = ({ image, userName, alt, save }) => Handlebars.compile(
     `   
         <div class="user-avatar">
-            <div class="user-avatar__change-avatar">
+            <form class="user-avatar__change-avatar">
                 <label class="user-avatar__label" for="avatar">Изменить аватар:</label>
                 <input class="user-avatar__input" type="file" name="avatar">
                 <div class="user-avatar__avatar-save">
                     {{{save}}}
                 </div>
-            </div>
+            </form>
             <img class="user-avatar__avatar" src={{image}} alt={{alt}} />
             <p class="user-avatar__name">{{userName}}</p>
         </div>
