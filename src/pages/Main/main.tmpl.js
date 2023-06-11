c652b6fb3662067f2ba6bb7d873b0af0acba7593
vasilevskaya <Vasilevskaya@gnivc.ru>
2023-06-11 21:35:57 +0300

    правки редиректа

--- src/pages/Main/main.tmpl.js
+++ src/pages/Main/main.tmpl.js
@@ -1,15 +1,17 @@
 export const template = `
-    <div class='main-page wrapper'>
+    <div class='main__page wrapper'>
         <h1>{{{title}}}</h1>
         <main>
-            <ul>
-                <li>{{{linkLogIn}}}</li>
-                <li>{{{linkSignup}}}</li>
-                <li>{{{linkProfile}}}</li>
-                <li>{{{linkNotFound}}}</li>
-                <li>{{{linkError}}}</li>
-                <li>{{{linkChats}}}</li>
-            </ul>
+            <nav>
+                <ul>
+                    <li>{{{linkLogIn}}}</li>
+                    <li>{{{linkSignup}}}</li>
+                    <li>{{{linkProfile}}}</li>
+                    <li>{{{linkNotFound}}}</li>
+                    <li>{{{linkError}}}</li>
+                    <li>{{{linkChats}}}</li>
+                </ul>
+            </nav>
         </main>
     </div>
 `
