ea54bbdf61c9e10c5ad6dfce02512e6f2c260d51
vasilevskaya <Vasilevskaya@gnivc.ru>
2023-06-12 02:09:41 +0300

    fix

--- src/pages/ChangePassword/index.js
+++ src/pages/ChangePassword/index.js
@@ -8,10 +8,17 @@ import { template } from './ChangePassword.tmpl';
 export const ChangePassword = () =>
     Handlebars.compile(template)({
         title: 'Изменить пароль',
-        oldPassword: Input({ label: 'Старый пароль', name: 'oldPassword' }),
-        newPassword: Input({ label: 'Новый пароль', name: 'newPassword'}),
-        newPasswordCopy: Input({ label: 'Повторите новый пароль', name: 'newPasswordCopy'}),
+        oldPassword: Input({ 
+            label: 'Старый пароль', 
+            name: 'oldPassword' 
+        }),
+        newPassword: Input({ 
+            label: 'Новый пароль', 
+            name: 'newPassword'
+        }),
+        newPasswordCopy: Input({ 
+            label: 'Повторите новый пароль', 
+            name: 'newPasswordCopy'
+        }),
         save: Button({ text: 'Сохранить' }),
     });
-    
-    
