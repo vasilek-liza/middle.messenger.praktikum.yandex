d794e417f679572ad64bea56df1c4695a0ba3ee1
vasilevskaya <Vasilevskaya@gnivc.ru>
2023-07-24 13:30:26 +0300

    fix serverjs

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(path.join(__dirname, './dist')));
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}./dist/index.html`);
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+app.use((req, res) => {
+  res.status(404).send('./dist');
 });
+
+app.listen(process.env.PORT || PORT);
+
