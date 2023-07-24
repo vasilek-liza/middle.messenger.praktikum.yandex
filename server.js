2251c2253b1911365caf38c6ec8237ca7ba3aead
vasilevskaya <Vasilevskaya@gnivc.ru>
2023-07-24 13:07:12 +0300

    fix server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-import express from 'express';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use(express.static('./dist'));
-
-app.get('*', (req, res) => {
-  res.sendFile('./dist/index.html', { root: __dirname });
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.use((req, res) => {
@@ -18,5 +15,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, function () {
-  console.log('go');
+  console.log('running');
 });
