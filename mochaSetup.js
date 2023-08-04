08bb3d85ebd17b7e9ccc5be0a4485dadc2c01435
vasilevskaya <Vasilevskaya@gnivc.ru>
2023-08-04 21:37:18 +0300

    test

---
+++ mochaSetup.js
@@ -0,0 +1,13 @@
+const { JSDOM } = require('jsdom');
+
+const { window } = new JSDOM('<div id="app"></div>', {
+    url: 'http://localhost:5173'
+});
+
+global.window = window;
+global.document = window.document;
+global.DocumentFragment = window.DocumentFragment;
+
+require.extensions['.scss'] = function () {
+    module.exports = () => ({});
+}
